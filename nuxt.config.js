a80d13068fece205696d5060ae2c757aa9cf892b
ibnuhazar101 <ibnuhazar101@gmail.com>
2023-08-14 08:48:48 +0700

    add credentials axios options

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,6 +32,7 @@ export default {
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: process.env.BASE_URL,
+    credentials: true,
   },
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
