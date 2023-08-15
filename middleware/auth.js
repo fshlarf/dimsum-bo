706994afb5e50205106f8e2f50e985ae60b3b2f4
ibnuhazar101 <ibnuhazar101@gmail.com>
2023-08-15 11:57:44 +0700

    apply auth middleware only in client rendering

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,8 +1,10 @@
 export default async function ({ app, $axios }) {
-  try {
-    const user = await $axios.get("/users/me");
-    console.log("user: ", user.data.data.email);
-  } catch (error) {
-    console.log(error);
+  if (process.client) {
+    try {
+      const user = await $axios.get("/users/me");
+      console.log("user: ", user.data.data.email);
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
