8d3b99fedbc0fa654bb4af83b484147c031a14d1
ibnuhazar101 <ibnuhazar101@gmail.com>
2023-08-15 09:55:11 +0700

    fix auth middleware

--- plugins/unauthorized.js
+++ plugins/unauthorized.js
@@ -1,13 +1,13 @@
 import showSnackbar from "~/helpers/snackbar";
 
-export default function ({ app, $axios }, redirect) {
+export default function ({ app, $axios, redirect }) {
   $axios.onError((error) => {
     if (error?.response?.status === 401) {
       showSnackbar({
         message: "Sesi berakhir",
         isSuccess: false,
       });
-      app.router.push("/login");
+      return redirect("/login");
     } else if (error?.response?.status === 500) {
       console.log(error?.response?.data?.error || error?.message || error);
       showSnackbar({
